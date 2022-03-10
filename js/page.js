649b562859b72f0878bb4cdd056fe30e502f4f7e
hsiao-i <eva090029@gmail.com>
2022-03-10 17:01:34 +0800

    20200310 revise

---
+++ js/page.js
@@ -0,0 +1,30 @@
+const awards = document.querySelector('.awards');
+awards.addEventListener('click', function(e) {
+    window.location.href = '#awards';
+    
+})
+
+const lesson = document.querySelector('.lesson');
+lesson.addEventListener('click', function(e) {
+    window.location.href = '#lesson';
+})
+
+const scientific = document.querySelector('.scientific');
+scientific.addEventListener('click', function(e) {
+    window.location.href = '#scientific';
+})
+
+const summerschool = document.querySelector('.summerschool');
+summerschool.addEventListener('click', function(e) {
+    window.location.href = '#summerschool';
+})
+
+const homepage = document.querySelector('.homepage');
+homepage.addEventListener('click', function(e) {
+    window.location.href = '#';
+})
+
+const profwu = document.querySelector('.profwu');
+profwu.addEventListener('click', function(e) {
+    window.location.href = '#profwu';
+})
